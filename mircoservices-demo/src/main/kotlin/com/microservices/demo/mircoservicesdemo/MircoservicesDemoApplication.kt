7a09dcdbd25d42bbaacdc3904cc25b23e9caea4b
솜놜 <90139018+somnwal@users.noreply.github.com>
2022-12-19 22:59:23 +0900

    20221219

---
+++ mircoservices-demo/src/main/kotlin/com/microservices/demo/mircoservicesdemo/MircoservicesDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.microservices.demo.mircoservicesdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MircoservicesDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MircoservicesDemoApplication>(*args)
+}
---
+++ mircoservices-demo/src/test/kotlin/com/microservices/demo/mircoservicesdemo/MircoservicesDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.demo.mircoservicesdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MircoservicesDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
