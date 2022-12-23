4a18235a41a35e378f82ebf56d6122781506e919
솜놜 <90139018+somnwal@users.noreply.github.com>
2022-12-24 05:04:15 +0900

    20221224

--- mircoservices-demo/twitter-to-kafka-service/src/main/kotlin/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.kt
+++ mircoservices-demo/twitter-to-kafka-service/src/main/kotlin/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.kt
@@ -1,7 +1,22 @@
 package com.microservices.demo.twitter.to.kafka.service
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Scope
+import java.sql.DriverManager.println
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class TwitterToKafkaServiceApplication {
+class TwitterToKafkaServiceApplication: CommandLineRunner {
+    fun main(args: Array<String>) {
+        runApplication<TwitterToKafkaServiceApplication>(*args)
+
+    }
+
+    override fun run(vararg args: String?) {
+        println("App Starts")
+    }
 }
+
